ac69c7753c5cda1b3983bb336653b7ce269000d1
Santhosh <santhosh.nitdgp2015@gmail.com>
2019-08-29 17:26:50 +0530

    Converting js assets to es5 - compilation fail

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,55 +1,49 @@
-(function(window, $){
+(function(window, $) {
+  const Login = function(config) {
+    const oThis = this;
 
-	const Login = function(config) {
-		const oThis = this;
-
-		$.extend(oThis.config, config);
+    $.extend(oThis.config, config);
     oThis.bindEvents();
-	};
+  };
 
-	Login.prototype = {
-
+  Login.prototype = {
     bindEvents: function() {
-    	const oThis = this;
+      const oThis = this;
 
       $('#login-btn').click(function(event) {
+        event.preventDefault();
 
-      	event.preventDefault();
+        var data = $('#login-form').serializeArray({});
 
-      	let data = $("#login-form").serializeArray({});
+        var postData = {};
 
-      	let postData = {};
+        for (var i = 0; i < data.length; i++) {
+          postData[data[i].name] = data[i].value;
+        }
 
-      	for(let i = 0; i < data.length; i++) {
-      		postData[data[i].name] = data[i].value;
-      	}
-
-	      $.ajax({
+        $.ajax({
           url: oThis.loginPostUrl(),
           type: 'POST',
           data: JSON.stringify(postData),
-          contentType: "application/json",
-          success: function (response) {
-
-            if(response.data) {
-							window.location = "/admin/user-approval"
-            } else {         
- 							console.error("=======Unknown response====");
+          contentType: 'application/json',
+          success: function(response) {
+            if (response.data) {
+              window.location = '/admin/user-approval';
+            } else {
+              console.error('=======Unknown response====');
             }
           },
           error: function(error) {
-          	console.error("===error", error);
+            console.error('===error', error);
           }
         });
-
       });
     },
 
     loginPostUrl: function() {
-    	return '/api/v1/admin/login';
+      return '/api/v1/admin/login';
     }
+  };
 
-	};
-
-	window.Login = Login;	
+  window.Login = Login;
 })(window, jQuery);
--- assets/js/userApproval.js
+++ assets/js/userApproval.js
@@ -1,27 +1,24 @@
-(function(window, $){
+(function(window, $) {
+  const UserApproval = function(config) {
+    const oThis = this;
 
-	const UserApproval = function(config) {
-		const oThis = this;
-
-		$.extend(oThis.config, config);
+    $.extend(oThis.config, config);
     oThis.bindEvents();
 
     oThis.paginationStack = [];
     oThis.lastPaginationId = null;
     oThis.query = null;
-	};
+  };
 
-	UserApproval.prototype = {
-
+  UserApproval.prototype = {
     bindEvents: function() {
-    	const oThis = this;
+      const oThis = this;
 
       // Trigger search
       $('#search-btn').click(function(event) {
+        event.preventDefault();
 
-      	event.preventDefault();
-
-      	let data = $("#user-search-form").serialize();
+        var data = $('#user-search-form').serialize();
 
         oThis.query = data;
 
@@ -34,7 +31,6 @@
       $('#users-previous-page').click(function(event) {
         event.preventDefault();
 
-        
         // Pop next page's, current page's(if present) pagination id and then get prev page pagination id
         oThis.paginationStack.pop();
 
@@ -42,11 +38,11 @@
           oThis.paginationStack.pop();
         }
 
-        let pagination_id = oThis.paginationStack.pop();
+        var pagination_id = oThis.paginationStack.pop();
 
-        let query = oThis.query;
+        var query = oThis.query;
         if (pagination_id) {
-          query = query + `&pagination_identifier=${pagination_id}`;
+          query = query + '&pagination_identifier=' + pagination_id;
         }
 
         oThis.loadUsers(query);
@@ -56,7 +52,7 @@
       $('#users-next-page').click(function(event) {
         event.preventDefault();
 
-        let query = oThis.query + `&pagination_identifier=${oThis.lastPaginationId}`;
+        var query = oThis.query + '&pagination_identifier=' + oThis.lastPaginationId;
 
         oThis.loadUsers(query);
       });
@@ -70,125 +66,125 @@
 
       // Disable or enable previous page button
       if (oThis.paginationStack.length == 0) {
-        $("#users-previous-page").attr("aria-disabled", "true");
-        $("#users-previous-page").css("pointer-events","none");
+        $('#users-previous-page').attr('aria-disabled', 'true');
+        $('#users-previous-page').css('pointer-events', 'none');
       } else {
-        $("#users-previous-page").attr("aria-disabled", "false");
-        $("#users-previous-page").removeClass("disabled");
-        $("#users-previous-page").css("pointer-events","auto");
+        $('#users-previous-page').attr('aria-disabled', 'false');
+        $('#users-previous-page').removeClass('disabled');
+        $('#users-previous-page').css('pointer-events', 'auto');
       }
 
+      // Don't use success callback function directly. Think of oThis.
       $.ajax({
-          url: oThis.adminUserSearchUrl(),
-          type: 'GET',
-          data: data,
-          contentType: "application/json",
-          success: function(response) {
-            oThis.userSearchSuccessCallback(response); // Don't use the function directly. Think of oThis.
-          },
-          error: function(error) {
-            console.error("===error", error);
-          }
+        url: oThis.adminUserSearchUrl(),
+        type: 'GET',
+        data: data,
+        contentType: 'application/json',
+        success: function(response) {
+          oThis.userSearchSuccessCallback(response);
+        },
+        error: function(error) {
+          console.error('===error', error);
+        }
       });
     },
 
     userSearchSuccessCallback: function(response) {
       const oThis = this;
 
-      let source   = document.getElementById("user-row").innerHTML;
-      let userRowTemplate = Handlebars.compile(source);
+      var source = document.getElementById('user-row').innerHTML;
+      var userRowTemplate = Handlebars.compile(source);
 
-      if(response.data) {
-
-        let searchResults = response.data[response.data.result_type];
+      if (response.data) {
+        var searchResults = response.data[response.data.result_type];
 
         // Handle pagination
-        let nextPageId = response.data.meta.next_page_payload ? response.data.meta.next_page_payload['pagination_identifier'] : null;
+        var nextPageId = response.data.meta.next_page_payload
+          ? response.data.meta.next_page_payload['pagination_identifier']
+          : null;
 
         if (searchResults.length == 0) {
           $('#user-search-results').append('<br/><p class="text-danger">No result found.</p>');
         }
 
         if (nextPageId) {
-          $("#users-next-page").attr("aria-disabled", "false");
-          $("#users-next-page").removeClass("disabled");
-          $("#users-next-page").css("pointer-events","auto");
+          $('#users-next-page').attr('aria-disabled', 'false');
+          $('#users-next-page').removeClass('disabled');
+          $('#users-next-page').css('pointer-events', 'auto');
 
           oThis.paginationStack.push(nextPageId);
           oThis.lastPaginationId = nextPageId;
         } else {
-          $("#users-next-page").attr("aria-disabled", "true");
-          $("#users-next-page").addClass("disabled");
-          $("#users-next-page").css("pointer-events","none");
+          $('#users-next-page').attr('aria-disabled', 'true');
+          $('#users-next-page').addClass('disabled');
+          $('#users-next-page').css('pointer-events', 'none');
 
           oThis.lastPaginationId = null;
         }
 
-        for (let ind = 0; ind < searchResults.length; ind++) {
+        for (var ind = 0; ind < searchResults.length; ind++) {
+          var userId = searchResults[ind]['payload'].user_id;
+          var video_id = searchResults[ind]['payload'].video_id;
+          var link_id = searchResults[ind]['payload'].link_id;
 
-          let userId = searchResults[ind]['payload'].user_id;
-          let video_id = searchResults[ind]['payload'].video_id;
-          let link_id = searchResults[ind]['payload'].link_id;
-
-          let userData = response.data['users'][userId];
+          var userData = response.data['users'][userId];
 
           // Get video link
-          let videoLink = '';
+          var videoLink = '';
           if (!response.data['videos'][video_id]) {
             // Nothing to do
           } else {
-            videoLink = response.data['videos'][video_id].resolutions['720w'] ? 
-              response.data['videos'][video_id].resolutions['720w'].url : 
-              response.data['videos'][video_id].resolutions['original'].url;
+            videoLink = response.data['videos'][video_id].resolutions['720w']
+              ? response.data['videos'][video_id].resolutions['720w'].url
+              : response.data['videos'][video_id].resolutions['original'].url;
           }
 
-          let status = userData.is_creator ? 'Approved' : 'Pending';
+          var status = userData.is_creator ? 'Approved' : 'Pending';
 
-          if (userData.status == "BLOCKED") {
+          if (userData.status == 'BLOCKED') {
             status = 'Blocked';
           }
 
           // Get social link
-          let socialLink = '';
+          var socialLink = '';
           if (!response.data['links'][link_id]) {
             // Nothing to do
           } else {
             socialLink = response.data['links'][link_id].url;
-          }      
+          }
 
-          let context = {
+          var context = {
             userId: userId,
-            name:  userData.name,
+            name: userData.name,
             userName: userData.user_name,
             status: status,
             videoLink: videoLink,
-            socialLink: socialLink,
+            socialLink: socialLink
           };
 
-          let html = userRowTemplate(context);
+          var html = userRowTemplate(context);
 
           $('#user-search-results').append(html);
         }
 
         oThis.bindVideoModalEvents();
         oThis.bindUserStateChangeEvents();
-           
-      } else {         
-        console.error("=======Unknown response====", response);
+      } else {
+        console.error('=======Unknown response====', response);
       }
     },
 
     bindVideoModalEvents: function() {
       const oThis = this;
 
-      let videoSource   = document.getElementById("video-tray").innerHTML;
-      let videoTemplate = Handlebars.compile(videoSource);
+      var videoSource = document.getElementById('video-tray').innerHTML;
+      var videoTemplate = Handlebars.compile(videoSource);
 
       // Add listner for video link click
       $('tr td a#video-link').click(function(event) {
         event.preventDefault();
 
-        let videoLink = $(this).attr('href');
+        var videoLink = $(this).attr('href');
 
         $('#modal-container').html(videoTemplate({ videoLink: videoLink }));
 
@@ -202,11 +198,9 @@
         // Stop playing video
         $('#modal-container').on('hidden.bs.modal', function(event) {
           $('.modal-backdrop').remove();
-          $('#modal-container').html('')
+          $('#modal-container').html('');
         });
-
       });
-
     },
 
     bindUserStateChangeEvents: function() {
@@ -217,18 +211,18 @@
 
         event.preventDefault();
 
-        let radioValue = $("input[name='userCreatorState']:checked").val();
-        let user_id = +($(this).attr('data-user-id'));
+        var radioValue = $("input[name='userCreatorState']:checked").val();
+        var user_id = +$(this).attr('data-user-id');
 
-        let updateButtonStatus = function() {
+        var updateButtonStatus = function() {
           $(button).text = 'Saved';
           $(button).addClass('disabled');
-          $(button).css("pointer-events","none");
-        }
+          $(button).css('pointer-events', 'none');
+        };
 
-        if (radioValue == "1") {
+        if (radioValue == '1') {
           oThis.approveUserAsCreator(user_id, updateButtonStatus);
-        } else if (radioValue == "2") {
+        } else if (radioValue == '2') {
           oThis.blockUser(user_id, updateButtonStatus);
         }
       });
@@ -241,17 +235,16 @@
         url: oThis.approveUserAsCreatorUrl(user_id),
         type: 'POST',
         data: {},
-        contentType: "application/json",
-        success: function (response) {
-
-          if(response.data) {
-            successCallback();            
-          } else {         
-            console.error("=======Unknown response====");
+        contentType: 'application/json',
+        success: function(response) {
+          if (response.data) {
+            successCallback();
+          } else {
+            console.error('=======Unknown response====');
           }
         },
         error: function(error) {
-          console.error("===error", error);
+          console.error('===error', error);
         }
       });
     },
@@ -263,34 +256,32 @@
         url: oThis.blockUserUrl(user_id),
         type: 'POST',
         data: {},
-        contentType: "application/json",
-        success: function (response) {
-
-          if(response.data) {
+        contentType: 'application/json',
+        success: function(response) {
+          if (response.data) {
             successCallback();
-          } else {         
-            console.error("=======Unknown response====");
+          } else {
+            console.error('=======Unknown response====');
           }
         },
         error: function(error) {
-          console.error("===error", error);
+          console.error('===error', error);
         }
       });
     },
 
     adminUserSearchUrl: function() {
-    	return '/api/v1/admin/users';
+      return '/api/v1/admin/users';
     },
 
     approveUserAsCreatorUrl: function(user_id) {
-      return `/api/v1/admin/users/${user_id}/approve`;
+      return '/api/v1/admin/users/' + user_id + '/approve';
     },
 
     blockUserUrl: function(user_id) {
-      return `/api/v1/admin/users/${user_id}/block`;
+      return '/api/v1/admin/users/' + user_id + '/block';
     }
+  };
 
-	};
-
-	window.UserApproval = UserApproval;	
+  window.UserApproval = UserApproval;
 })(window, jQuery);
