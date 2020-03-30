03680d7353e2130d912a5c827ec2929c60acadfa
alpeshvmodi <alpeshvmodi@gmail.com>
2020-03-30 16:36:53 +0530

    channel page from admin panel

--- assets/js/channel.js
+++ assets/js/channel.js
@@ -7,6 +7,10 @@
     oThis.apiUrl = $('meta[name="api-url"]').attr('content');
 
     oThis.createEditBtn = $('#create-edit-channel');
+    oThis.imageUploadParams = {};
+    oThis.imageNames = [];
+
+    oThis.getPresignedPostUrl();
 
     oThis.bindEvents();
   };
@@ -31,31 +35,108 @@
           $(oThis.createEditBtn).removeClass('disabled');
         };
 
-        oThis.createEditChannel(successCallback);
+        oThis.uploadImages(successCallback, successCallback);
       });
     },
 
-    createEditChannel: function(successCallback) {
+    getPresignedPostUrl: function() {
       var oThis = this;
 
-      alert('yyehhhha se request mardo');
-      return;
+      $.ajax({
+        url: oThis.apiUrl + '/admin/channel/presigned-url',
+        type: 'GET',
+        contentType: 'application/json',
+        success: function(response) {
+          if (response.data) {
+            console.log(response.data);
+
+            if (response.data.channel_upload_params && response.data.channel_upload_params.images) {
+              const imagesToUpload = response.data.channel_upload_params.images;
+              console.log('----------------imagesToUpload-------------------------------');
+              console.log(imagesToUpload);
+
+              for (let imageName in imagesToUpload) {
+                oThis.imageNames.push(imageName);
+                oThis.imageUploadParams[imageName] = oThis.imageUploadParams[imageName] || {};
+                oThis.imageUploadParams[imageName]['post_url'] = imagesToUpload[imageName].post_url;
+                const post_fields = imagesToUpload[imageName].post_fields;
+
+                for (let imu = 0; imu < post_fields.length; imu++) {
+                  oThis.imageUploadParams[imageName]['post_fields'] =
+                    oThis.imageUploadParams[imageName]['post_fields'] || {};
+                  oThis.imageUploadParams[imageName]['post_fields'][post_fields[imu].key] = post_fields[imu].value;
+                }
+              }
+            }
+
+            console.log('----------------imageUploadParams-------------------------------');
+            console.log(oThis.imageUploadParams);
+          } else {
+            console.error('=======Unknown response====.serialize', response);
+          }
+        },
+        error: function(error) {
+          console.error('===error', error);
+        }
+      });
+    },
+
+    uploadImages: function(successCallback, successCallback) {
+      const oThis = this;
+
+      const originalFiles = document.getElementById('originalImage').files;
+
+      if (originalFiles.length > 0) {
+        const originalFile = originalFiles[0];
+        const originalFileSize = originalFile.size;
+        const uploadImageName = oThis.imageNames[0];
+        originalFile.name = uploadImageName;
+
+        const imagePostUrl = oThis.imageUploadParams[uploadImageName]['post_url'];
+        const imageUploadParams = oThis.imageUploadParams[uploadImageName]['post_fields'];
+
+        console.log('--------------------------------------------', imageUploadParams);
+      }
+
+      oThis.createEditChannel(successCallback, successCallback);
+    },
+
+    createEditChannel: function(successCallback, failureCallback) {
+      var oThis = this;
+
+      var data = $('#channel-form').serializeArray({});
+
+      var postData = {};
+
+      for (var i = 0; i < data.length; i++) {
+        if (data[i].value) {
+          postData[data[i].name] = data[i].value;
+        }
+      }
+
       // send ajax to api to create edit channel.
       $.ajax({
         url: oThis.apiUrl + '/admin/channel/edit',
         type: 'POST',
-        data: {},
+        data: JSON.stringify(postData),
         contentType: 'application/json',
         success: function(response) {
           if (response.data) {
+            $('#requestSuccess').html('Request Successful.');
+            $('#requestSuccess').show();
             successCallback();
           } else {
+            $('#requestError').html(JSON.stringify(response.err));
+            $('#requestError').show();
             console.error('=======Unknown response====', response);
           }
         },
         error: function(error) {
           console.error('===error', error);
+          $('#requestError').html('Request Failed.');
+          $('#requestError').show();
 
+          failureCallback();
           if (error.responseJSON.err.code == 'UNAUTHORIZED') {
             window.location = '/admin/unauthorized';
           }
